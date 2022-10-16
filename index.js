d5b8d50f758e7ef3b62b95cd0a65b34d14d3b12f
Your Name <senasezer@sena-MacBook-Air.local>
2022-10-16 22:40:38 +0300

    done

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase( ));
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase( ));
+}
+function sayHiToHeadphonedRoommate(string) {
+    var text= "I can't hear you!"
+    var other= "YES INDEED!"
+    var final= "I would love to!"
+    if (string.toLowerCase(string) === string) {
+        return text;
+      }
+      else if (string.toUpperCase(string) === string) {
+        return other;
+      }
+      else if ("Let's have dinner together!" === string) {
+        return final
+      }
+    }
